ec35b1191c19caf9d94eca9bd0efe3bc8fc94654
aldm <d44.a1@yandex.ru>
2024-07-17 11:55:03 +0300

    1

---
+++ js/swiper.js
@@ -0,0 +1,21 @@
+const swiper = new Swiper(".promo__swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: false,
+
+  // If we need pagination
+  pagination: {
+    el: ".promo__pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".promo__next",
+    prevEl: ".promo__prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
