756b3cc0f15782454a2183a09be3b8d8e4032dd8
aldm <d44.a1@yandex.ru>
2024-07-22 18:15:29 +0300

    1

--- js/index.js
+++ js/index.js
@@ -7,3 +7,10 @@ links.forEach((link) => {
     this.classList.add("active");
   });
 });
+
+//мобильное меню
+const headerBurger = document.querySelector(".header__button");
+
+headerBurger.addEventListener("click", () => {
+  headerBurger.classList.toggle("header__button_open");
+});
